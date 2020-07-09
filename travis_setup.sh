396c4937b99704b61cb05e6f812bc8c332f6682d
nmcdonnell-kx <nmcdonnell@kx.com>
2020-07-09 17:51:37 +0100

    gfd

--- travis_setup.sh
+++ travis_setup.sh
@@ -6,23 +6,23 @@ mkdir cbuild/install
 wget https://github.com/protocolbuffers/protobuf/releases/download/v3.12.3/protobuf-cpp-3.12.3.tar.gz
 tar xvf protobuf-cpp-3.12.3.tar.gz -C ./cbuild --strip-components=1
 
-if [ "$TRAVIS_OS_NAME" == "osx" || "$TRAVIS_OS_NAME" == "linux" ]; then
-    cd cbuild
-    ./configure --prefix=./install "CFLAGS=-fPIC" "CXXFLAGS=-fPIC"
-    make
-    make install
-    # Make sure protoc.exe is on the path so the cmake functionality to locate the protobuf installation works
-    set PATH=./install:$PATH
-    cd ..
-elif [ "$TRAVIS_OS_NAME" == "windows" ]; then
-    mkdir cbuild/cmake/solution
-    cd cbuild/cmake/solution
-    cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_INSTALL_PREFIX=../../install ../..
-    cmake --build . --config Release
-    cmake --build . --config Release --target install
-    # Make sure protoc.exe is on the path so the cmake functionality to locate the protobuf installation works
-    set PATH=../../install:$PATH
-    cd ../../..
+if [[ "$TRAVIS_OS_NAME" == "osx" || "$TRAVIS_OS_NAME" == "linux" ]]; then
+  cd cbuild
+  ./configure --prefix=./install "CFLAGS=-fPIC" "CXXFLAGS=-fPIC"
+  make
+  make install
+  # Make sure protoc.exe is on the path so the cmake functionality to locate the protobuf installation works
+  set PATH=./install:$PATH
+  cd ..
+elif [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
+  mkdir cbuild/cmake/solution
+  cd cbuild/cmake/solution
+  cmake -G "Visual Studio 15 2017 Win64" -DCMAKE_INSTALL_PREFIX=../../install ../..
+  cmake --build . --config Release
+  cmake --build . --config Release --target install
+  # Make sure protoc.exe is on the path so the cmake functionality to locate the protobuf installation works
+  set PATH=../../install:$PATH
+  cd ../../..
 else
-    echo "$TRAVIS_OS_NAME is currently not supported"  
+  echo "$TRAVIS_OS_NAME is currently not supported"  
 fi
